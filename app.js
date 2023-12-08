21042a10c440508590a923e6f1626de25d2957b5
Kyle Seto <kyle@MacBook-Pro.fios-router.home>
2023-12-08 14:23:42 -0500

    Added session code

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 import express from 'express';
 import cors from "cors";
 import mongoose from "mongoose";
+import session from "express-session";
 import UserRoutes from "./users/routes.js";
 import FollowsRoutes from "./follows/routes.js";
 import LikesRoutes from "./likes/routes.js";
 mongoose.connect("mongodb://127.0.0.1:27017/Lodger");
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+   );
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  app.use(
+    session(sessionOptions)
+  );
+  
 app.use(express.json());
 
 FollowsRoutes(app);
