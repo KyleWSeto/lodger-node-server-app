302379f3d3f64b5ccac97e51c9aab5afe1fdcac6
Kyle Seto <kyle@MacBook-Pro.fios-router.home>
2023-12-08 19:46:44 -0500

    Update user schema

--- users/schema.js
+++ users/schema.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
+    username: String,
+    password: String,
     contact: String,
     interests: String,
     travelWishList: String,
