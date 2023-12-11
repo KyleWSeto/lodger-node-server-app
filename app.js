4e3a18fbbea5e7a4cd5143d9050b015852e1e0a6
Kyle Seto <kyle@MacBook-Pro.fios-router.home>
2023-12-10 20:38:27 -0500

    Remove tutorial code

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-/*
-Some code here are from a tutorial about a community-based prototype endorsed by the Amadeus API.
-Such code will be marked. For full details, visit...
-Tutorial page: https://developers.amadeus.com/blog/react-hotel-booking-app-part1
-Github repository: https://github.com/amadeus4dev-examples/Building-a-Hotel-Booking-App-in-NodeJS-and-React-Part-1
-*/
-
 import express from 'express';
 import cors from "cors";
 import mongoose from "mongoose";
@@ -13,7 +6,6 @@ import UserRoutes from "./users/routes.js";
 import FollowsRoutes from "./follows/routes.js";
 import LikesRoutes from "./likes/routes.js";
 import ReviewsRoutes from "./reviews/routes.js";
-import router from "./router.js";  //
 mongoose.connect("mongodb://127.0.0.1:27017/Lodger");
 
 const app = express();
@@ -33,7 +25,6 @@ const sessionOptions = {
   );
   
 app.use(express.json());
-app.use("/", router);  //
 
 FollowsRoutes(app);
 LikesRoutes(app);
